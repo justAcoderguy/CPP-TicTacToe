65e80fac24157052a9b29cbc98d468d5c0144496
justAcoderguy <abhishek.banerjee18@outlook.com>
2023-10-08 13:31:09 +0100

    Added game and render logic

--- Main.cpp
+++ Main.cpp
@@ -22,7 +22,7 @@ class Board
         BoardSquare::E* _squares;
 
     public:
-        // constructor using Init list ( skips internal steps, is faster )
+        // constructor using Init list ( skips internal steps; is faster )
         // Takes in width and assigns it to local _width
         Board(int width) : _width(width)
         {
@@ -60,8 +60,72 @@ class IRuleEngine
 
 class Game
 {
+    private:
+        Board& _board;
+        IRuleEngine& _ruleEngine;
     public:
-        BoardSquare::E Run() {}
+        Game(Board& board, IRuleEngine& ruleEngine) : _board(board), _ruleEngine(ruleEngine) {}
+
+        BoardSquare::E Run() 
+        {
+            BoardSquare::E winningPlayer, currentPlayer = BoardSquare::E::X;
+            // While no one has won
+            while((winningPlayer = _ruleEngine.HasWon(_board)) == BoardSquare::E::Empty)
+            {
+                Render();
+                cout << "\n";
+
+                int input;
+                cout << "Move for " << GetPlayerChar(currentPlayer) << ": ";
+                cin >> input;
+                cin.ignore();
+
+                input -= 1;
+                // Validating input 
+                if (input < 0 || input >= _board.GetTotalSquares() || _board.GetSquare(input) != BoardSquare::E::Empty)
+                {
+                    cout << "invalid Move!\n";
+                    continue;
+                }
+
+                _board.Setsquare(input, currentPlayer);
+                currentPlayer = currentPlayer == BoardSquare::E::X ? BoardSquare::E::O : BoardSquare::E::X;
+            }
+
+            return winningPlayer;
+        }
+
+    private:
+        void Render() 
+        {
+            for (auto i = 1; i <= _board.GetTotalSquares(); i++)
+            {
+                // index 1 based as for the user the board should start from '1'
+                cout << GetSquareChar(i, _board.GetSquare(i-1)) << " ";
+                if ((i % _board.Getwidth()) == 0)
+                    cout << "\n"; 
+            }
+        }
+
+        char GetPlayerChar (BoardSquare::E player)
+        {
+            switch (player)
+            {
+                case BoardSquare::E::X: return 'X';
+                case BoardSquare::E::O: return 'O';
+            }
+        }
+
+        char GetSquareChar (int index, BoardSquare::E square) 
+        {
+            switch(square)
+            {
+                case BoardSquare::E::X: return 'X';
+                case BoardSquare::E::O: return 'O';
+            }
+
+            return index + '0';
+        }
 };
 
 int main(){
